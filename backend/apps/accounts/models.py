ad3e866b94cf27d2176e27cbc3c5d6af795d697e
harinderkmr <harinderjim@gmail.com>
2026-07-14 18:33:58 +0530

    feat(auth): implement custom user model with email authentication

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,53 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_verified = models.BooleanField(
+        default=False,
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -148,3 +148,4 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
 }
+AUTH_USER_MODEL = "accounts.User"
